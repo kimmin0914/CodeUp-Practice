25e027bf61b747edeecc174e9ee0d3252a610a55
kimmin0914 <zktpxldk@gmail.com>
2026-05-18 14:46:23 +0900

    5월 3주차 문제풀이

---
+++ CodeUp-Practice-71~80/1076_46983828(AC).c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    char c, i = 'a';
+    scanf("%c", &c);
+    
+    do {
+        printf("%c ", i);
+        i++;
+    } while (i <= c);
+    
+    return 0;
+}
---
+++ CodeUp-Practice-71~80/1078_46983872(AC).c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    int n, sum = 0;
+    scanf("%d", &n);
+    
+    for (int i = 1; i <= n; i++)
+{
+        if (i % 2 == 0) {
+            sum += i;
+        }
+    }
+    
+    printf("%d\n", sum);
+    
+    return 0;
+}
